956ffe7ebc2b0cdb806deeef24dc998319094cfe
Michał Oniszczuk <michal.oniszczuk@gmail.com>
2012-05-23 12:18:44 +0200

    Refactoring: MonadReader: isoM cs cs' ~> isoM.

--- interpreter/Lucretia/TypeChecker/IsomorphicModuloNames.hs
+++ interpreter/Lucretia/TypeChecker/IsomorphicModuloNames.hs
@@ -5,6 +5,7 @@ module Lucretia.TypeChecker.IsomorphicModuloNames (iso) where
 import Prelude hiding (any)
 
 import Control.Monad.Error
+import Control.Monad.Reader
 import Control.Monad.State
 import Control.Monad.Identity
 
@@ -21,23 +22,23 @@ import Lucretia.TypeChecker.Types
 
 class IsomorphicModuloNames a where
   iso  :: (a, Constraints) -> (a, Constraints) -> Either String ()
-  iso (a, cs) (a', cs') = evalStateT (isoM cs cs' a a') Set.empty
-  isoM :: Constraints -> Constraints -> a -> a -> M ()
+  iso (a, cs) (a', cs') = evalStateT (runReaderT (isoM a a') (cs, cs')) Set.empty
+  isoM :: a -> a -> M ()
 
-type M = StateT Isomorphism (Either String)
+type M = ReaderT (Constraints, Constraints) (StateT Isomorphism (Either String))
 type Isomorphism = Set (Name, Name) -- visited node pairs
 
 isomorphic :: M ()
 isomorphic = return ()
 notIsomorphic :: String -> M ()
-notIsomorphic why = throwError why
+notIsomorphic = throwError
 
 instance IsomorphicModuloNames Type where
-  isoM cs cs' (TVar n) (TVar n') = isoM cs cs' n n'
-  isoM cs cs' a a' = unless (a == a') $ notIsomorphic $ "Expected type " ++ show a' ++ " but got " ++ show a ++ ".\n"
+  isoM (TVar n) (TVar n') = isoM n n'
+  isoM a a' = unless (a == a') $ notIsomorphic $ "Expected type " ++ show a' ++ " but got " ++ show a ++ ".\n"
 
 instance IsomorphicModuloNames Name where
-  isoM cs cs' n n' = do
+  isoM n n' = do
     v <- get
     if Set.member (n, n') v
       then isomorphic
@@ -47,11 +48,9 @@ instance IsomorphicModuloNames Name where
       v <- get
       guard $ not (memberFst n v) && not (memberSnd n' v)
       modify $ Set.insert (n, n')
-      -- TODO OPT RTR using lenses
-      -- http://stackoverflow.com/questions/8469044/template-haskell-with-record-field-name-as-variable
-      t  <- lookupM n  cs
-      t' <- lookupM n' cs'
-      isoM cs cs' t t'
+      t  <- lookupM n  =<< asks fst
+      t' <- lookupM n' =<< asks snd
+      isoM t t'
 
     memberFst :: (Eq a, Foldable f) => a -> f (a, b) -> Bool
     memberFst x = any $ \(y, _) -> x == y
@@ -65,10 +64,10 @@ instance IsomorphicModuloNames Name where
 	Nothing     -> throwError $ "Cannot find type variable named: " ++ n ++ " in constraints: " ++ showConstraints cs ++ ".\n"
 
 instance IsomorphicModuloNames (Map Name Type) where
-  isoM cs cs' = (isoM cs cs') `on` Map.toAscList
+  isoM = isoM `on` Map.toAscList
   --isoM m m' = Map.toAscList m `isoM` Map.toAscList m'
 
 instance IsomorphicModuloNames [(Name, Type)] where
-  isoM cs cs' ((_, t):xs) ((_, t'):xs') = isoM cs cs' t t' >> isoM cs cs' xs xs'
-  isoM cs cs' [] [] = isomorphic
+  isoM ((_, t):xs) ((_, t'):xs') = isoM t t' >> isoM xs xs'
+  isoM [] [] = isomorphic
 
