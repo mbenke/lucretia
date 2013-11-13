929a739a07086ca17eba607a5828068989fc941c
Michał Oniszczuk <michal.oniszczuk@gmail.com>
2013-11-13 12:02:01 +0100

    Moving all typing examples to TypeChecker/Test.hs

--- interpreter/Lucretia/Syntax.hs
+++ interpreter/Lucretia/Syntax.hs
@@ -8,30 +8,37 @@ type Defs = [Def]
 type Def = (Var, Exp)
 
 data Exp 
-    = EInt Integer
-    | EBool Bool
-    | EStr String
-    | ENone -- ^ () in wp
-    | ENew
+  = EInt Integer
+  | EBool Bool
+  | EStr String
+  | ENone -- ^ () in wp
+  | ENew
 
-    | EAdd Exp Exp
-    | EMul Exp Exp
+  | EAdd Exp Exp
+  | EMul Exp Exp
 
-    | EVar Var
-    | ELet Var Exp Exp
+  | EVar Var
+  | ELet Var Exp Exp
 
-    | EGet Var Field
-    | ESet Var Field Exp
+  | EGet Var Field
+  | ESet Var Field Exp
 
-    | EIf Exp Exp Exp
-    | EIfHasAttr Var Field Exp Exp
+  | EIf Exp Exp Exp
+  | EIfHasAttr Var Field Exp Exp
 
-    | EFunDecl [Var] Exp
-    | EFunCall Exp [Exp]
-
-      deriving (Eq,Show)
+  | EFunDecl [Var] Exp
+  | EFunCall Exp [Exp]
+  
+  deriving (Eq,Show)
 
 programFromExp :: Exp -> Program
 programFromExp e = [("_",e)]
 
+instance Num Exp where
+  fromInteger = EInt
+  (+) = EAdd
+  (*) = undefined
+  (-) = undefined
+  signum = undefined
+  abs = undefined
 
